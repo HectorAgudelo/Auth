269ffc0d2dcea9b6fe9072dd878e094b311d1090
Hector Agudelo <haacny86@gmail.com>
2023-02-06 09:57:21 -0500

    add firebase config file and install dependency

---
+++ src/config/config.tsx
@@ -0,0 +1,10 @@
+export const config = {
+  firebaseConfig : {
+  apiKey: "AIzaSyAhdPiovAeIBJYNtPNVHGo_PfqTnp5T6MU",
+  authDomain: "authapp-209f6.firebaseapp.com",
+  projectId: "authapp-209f6",
+  storageBucket: "authapp-209f6.appspot.com",
+  messagingSenderId: "534138234478",
+  appId: "1:534138234478:web:331843e9b992f4dbf8a87f"
+}
+};
